405daa55daa8eab75ad4ee0d120dac356b79e9fb
Christopher Carrasco <christopher3carrasco@gmail.com>
2025-03-18 16:11:34 -0500

    test: add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,86 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  // Test 1: Visualización de un stock
+  test('Viewing one stock: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body.stockData);
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        done();
+      });
+  });
 
+  // Test 2: Viendo una acción y me gusta
+  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body.stockData);
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.likes, 1); // Verifica que el like se haya registrado
+        done();
+      });
+  });
+
+  // Test 3: Viendo el mismo stock y me gustando nuevamente
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body.stockData);
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.likes, 1); // El like no debería incrementarse nuevamente
+        done();
+      });
+  });
+
+  // Test 4: Visualización de dos acciones
+  test('Viewing two stocks: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.property(res.body.stockData[0], 'stock');
+        assert.property(res.body.stockData[0], 'price');
+        assert.property(res.body.stockData[1], 'stock');
+        assert.property(res.body.stockData[1], 'price');
+        done();
+      });
+  });
+
+  // Test 5: Visualizando dos acciones y me gustan
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.property(res.body.stockData[0], 'stock');
+        assert.property(res.body.stockData[0], 'price');
+        assert.property(res.body.stockData[0], 'rel_likes');
+        assert.property(res.body.stockData[1], 'stock');
+        assert.property(res.body.stockData[1], 'price');
+        assert.property(res.body.stockData[1], 'rel_likes');
+        done();
+      });
+  });
 });
